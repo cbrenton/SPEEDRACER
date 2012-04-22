e0fc68f537a3302ae5b42038f3c0711290eb40cc
Chris Brenton <chrisb@ifixit.com>
2012-04-22 12:02:07 -0700

    "make lib" now creates a symbolic link to main.cpp called main.cu.

--- lib.sh
+++ lib.sh
@@ -3,5 +3,10 @@
 cd ./lib/pngwriter-0.5.4
 make DESTDIR=../pngwriter
 make install
-cd ../cubuild
-make
+#cd ../cubuild
+#make
+cd ../../src
+if [ ! -e main.cu ] && [ ! -h main.cu ]
+then
+   ln -s ./main.cpp ./main.cu
+fi
