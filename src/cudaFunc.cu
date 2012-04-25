3fffd9172c3b5037ce7c420795172a5dcd6b7bee
fenrir31 <fenrir31@gmail.com>
2012-04-25 13:07:20 -0700

    Updated cudaFunc to compile and assume struct array in point_t instead of pointers to pointers

--- src/cudaFunc.cu
+++ src/cudaFunc.cu
@@ -20,7 +20,7 @@ tri_t* retrieveTrianglesFromDevice(tri_t* tri_d,int size)
    cudaFree(tri_d);
    return tri_r;
 }
-point_t* retrievePointsFromDevice(point_t*pointList,point_t* point_d,int size)
+point_t* retrievePointsFromDevice(point_t* point_d,int size)
 {
    point_t* point_r;
    point_r = (point_t*)malloc(sizeof(point_t)*size);
@@ -40,24 +40,20 @@ point_t* sendPointsToDevice(point_t* pointList,int size)
 }
 
 //function to run the entire convert process
-tri_t* cudaConvertCoords(tri_t* triList,int size, int h, int w)
+point_t* cudaConvertCoords(point_t* pointList,int size, int h, int w,vec_t scale)
 {
-   tri_t* tri_d;// pointer for input list on device
-   tri_t* tri_r;// pointer for holding the result of calc
+   point_t* point_d;// pointer for holding the result of calc
    dim3 dimBlock(h/50 +1, w/50 +1);
    dim3 dimGrid(50,3);
-   tri_d= sendTrianglesToDevice(triList,size);
-   //cudaCoordinateCalc<<<dimBlock,dimGrid>>>(thrust::raw_pointer_cast(tri_d),tri_d.size(),
-  // thrust::raw_pointer_cast(tri_r),w,h);
-   
-   return retrieveTrianglesFromDevice(tri_r,size);
+   point_d= sendPointsToDevice(pointList,size);
+   cudaCoordinateCalc<<<dimBlock,dimGrid>>>(point_d, size, h, w,scale);
+   return retrievePointsFromDevice(point_d,size);
    
 }
 
-/*
+
 //kernel for finding the new points after conversion
-__global__ void cudaCoordinateCalc(tri_t* triList, int listSize
-   ,tri_t* tri_d,int w_in, int h_in,vec_t scale = (vec_t)1.0)
+__global__ void cudaCoordinateCalc(point_t* point_d, int listSize,int w_in, int h_in,vec_t scale )
 {
    int location;
    location =blockIdx.x*50 +threadIdx.x;
@@ -66,28 +62,19 @@ __global__ void cudaCoordinateCalc(tri_t* triList, int listSize
    {
       return;
    }
+   scale = (vec_t) 1.00;
    int tpX,tpY; 
    int h= h_in;
    int w= w_in;
-  // Convert x.
-   float tmpX = triList[location].p[threadIdx.x].x + dim; // Shift.
-   tpX = (int)(tmpX * (float)(w - 1) / 2 * (1.f / dim)); // Scale.
-   // Convert y.
-   float tmpY = y + dim; // Shift.
-   tpY = (int)(tmpY * (float)(h - 1) / 2 * (1.f / dim)); // Scale.
-
    vec_t dim = scale;
    // Convert x.
-   vec_t tmpX = triList[location].pt[threadIdx.y].coords.v[0] + dim; // Shift.
+   vec_t tmpX = point_d[location].coords.v[0] + dim; // Shift.
    tpX = (int)(tmpX * (vec_t)(w - 1) / 2 * ((vec_t)1.0 / dim)); // Scale.
    // Convert y.
-   vec_t tmpY = triList[location].pt[threadIdx.y].coords.v[1] + dim; // Shift.
+   vec_t tmpY = point_d[location].coords.v[1] + dim; // Shift. 
    tpY = (int)(tmpY * (vec_t)(h - 1) / 2 * ((vec_t)1.0 / dim)); // Scale.
-
-
-
-   ((tri_t)tri_d[location]).((point_t)pt[threadIdx.y]).px = tpX;
-   tri_d[location].pt[threadIdx.y].py = tpY;
+   point_d[location].pX = tpX;
+   point_d[location].pY = tpY;
    return;
 }
-*/
+
--- src/cudaFunc.h
+++ src/cudaFunc.h
@@ -10,10 +10,9 @@ using namespace std;
 
 
 //function that manages the entire conversion process
-__global__ void cudaCoordinateCalc(tri_t* triList,int listSize, tri_t* tri_d,int w_in, int h_in);
-
+__global__ void cudaCoordinateCalc(point_t* point_d, int listSize,int w_in, int h_in,vec_t scale );
 //kernel for converting the coordinates
-thrust::host_vector<tri_t> cudaConvertCoords(vector<tri_t> triList,int h, int w);
+point_t* cudaConvertCoords(point_t* pointList,int h, int w,vec_t scale);
 
 //function that sends the vector to the device
 tri_t* sendTrianglesToDevice(tri_t* triList,int size);
@@ -23,3 +22,5 @@ point_t* sendPointsToDevice(point_t* pointList,int size);
 //function that retrieves the data vector and returns a direct pointer to it
 tri_t* retrieveTrianglesFromDevice(tri_t* triList_d,int size);
 
+//function to retrieve the point array, returns a malloced result
+point_t* retrievePointsFromDevice(point_t* point_d,int size);
