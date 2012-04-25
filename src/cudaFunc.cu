8836fd9a8f180ab8326207e4ff4dbc84ac9b8f1a
daburke <daburke@235x27.csc.calpoly.edu>
2012-04-24 18:20:29 -0700

    updated cudafunc for pointer solutions as opposed to vectors

--- src/cudaFunc.cu
+++ src/cudaFunc.cu
@@ -2,37 +2,40 @@
 
 //using namespace std; 
 /*file for implementing the cuda version of rasterization functions*/
-//function for sending the vector to the device
-//__constant__ vector<tri_t>* tr_d;
-thrust::device_vector<tri_t> sendTrianglesToDevice(vector<tri_t> triList)
+//
+tri_t* sendTrianglesToDevice(tri_t* triList,int size)
 {
-   thrust::device_vector<tri_t> tri_d = triList;
+   tri_t* triList_d;
+   cudaMalloc(&triList_d,sizeof(tri_t)*size);
+   cudaMemCpy(triList_d,triList,sizeof(tri_t)*size,cudaMemcpyHostToDevice);
    return tri_d;
 }
 //Function for retrieving the converted tri_t after the kernel has been run
-thrust::host_vector<tri_t> retrieveCoordinatesFromDevice(thrust::device_vector<tri_t> tri_d)
+//assumes that the tri_return points to a malloced pointer for the given size
+tri_t* retrieveCoordinatesFromDevice(tri_t* triList_d,int size)
 {
-   thrust::host_vector<tri_t> tri_h= tri_d;
-   //tri_t * ptr = thrust::raw_pointer_cast(&tri_h[0]);
-
-   return tri_h;
+   tri_t* tri_r;
+   tri_r = malloc(sizeof(tri_t)*size);
+   cudaMemCpy(tri_return,triList_d,sizeof(tri_t)*size,cudaMemcpyDeviceToHost);
+   //cudaFree(triList_d);
+   return tri_return;
 }
 //function to run the entire convert process
-thrust::host_vector<tri_t> cudaConvertCoords(vector<tri_t> triList,int h, int w)
+tri_t* cudaConvertCoords(tri_t* triList,int size, int h, int w)
 {
-   thrust::device_vector<tri_t> tri_d;// vector for input list on device
-   thrust::device_vector<tri_t> tri_r;// vector for holding the result of calc
+   tri_t* tri_d;// pointer for input list on device
+   tri_t* tri_r;// pointer for holding the result of calc
    dim3 dimBlock(h/50 +1, w/50 +1);
    dim3 dimGrid(50,3);
-   tri_d= sendTrianglesToDevice(triList);
-   cudaCoordinateCalc<<<dimBlock,dimGrid>>>(thrust::raw_pointer_cast(tri_d),tri_d.size(),
-   thrust::raw_pointer_cast(tri_r),w,h);
+   tri_d= sendTrianglesToDevice(triList,size);
+   //cudaCoordinateCalc<<<dimBlock,dimGrid>>>(thrust::raw_pointer_cast(tri_d),tri_d.size(),
+  // thrust::raw_pointer_cast(tri_r),w,h);
   
-   return retrieveCoordinatesFromDevice(tri_r);
+   return retrieveCoordinatesFromDevice(tri_r,size);
    
 }
 
-
+/*
 //kernel for finding the new points after conversion
 __global__ void cudaCoordinateCalc(tri_t* triList, int listSize
    ,tri_t* tri_d,int w_in, int h_in,vec_t scale = (vec_t)1.0)
@@ -47,13 +50,13 @@ __global__ void cudaCoordinateCalc(tri_t* triList, int listSize
    int tpX,tpY; 
    int h= h_in;
    int w= w_in;
-/*   // Convert x.
+  // Convert x.
    float tmpX = triList[location].p[threadIdx.x].x + dim; // Shift.
    tpX = (int)(tmpX * (float)(w - 1) / 2 * (1.f / dim)); // Scale.
    // Convert y.
    float tmpY = y + dim; // Shift.
    tpY = (int)(tmpY * (float)(h - 1) / 2 * (1.f / dim)); // Scale.
-*/
+
    vec_t dim = scale;
    // Convert x.
    vec_t tmpX = triList[location].pt[threadIdx.y].coords.v[0] + dim; // Shift.
@@ -68,4 +71,4 @@ __global__ void cudaCoordinateCalc(tri_t* triList, int listSize
    tri_d[location].pt[threadIdx.y].py = tpY;
    return;
 }
-
+*/
